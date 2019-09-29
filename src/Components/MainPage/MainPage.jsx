6a71499c076b23c9a3db6ecf9ef9a9758534c305
Amy Williams <csamywilliams@gmail.com>
2019-09-29 12:01:24 +0100

    Initial commit

---
+++ src/Components/MainPage/MainPage.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+
+class MainPage extends Component {
+
+  render() {
+
+    return (
+        <main>
+        
+        </main>
+    );
+  }
+ 
+}
+
+export default MainPage;
---
+++ src/theme.jsx
@@ -0,0 +1,20 @@
+const paleOrange = '#EED093';
+const beige = '#FBF8F3';
+const green = '#8EBE68';
+const brown = '#2D1710';
+const white = '#fff';
+
+const color1 = beige;
+const color2 = paleOrange;
+const color3 = green;
+const color4 = beige;
+const color5 = green;
+
+const theme = {
+    main: {
+       
+
+    }
+};
+
+export default theme;
